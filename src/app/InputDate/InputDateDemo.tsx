7a4efd3c4b12032ddd737945409295e45fd46936
Unknown <alexandre.annic1@gmail.com>
2018-06-11 17:38:50 +0200

    init

---
+++ src/app/InputDate/InputDateDemo.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import Page from '../Page';
+import InputDate from '../../lib/InputDate/InputDate';
+
+class InputDateDemo extends React.Component<{}, {}> {
+
+  render() {
+    const style = {marginRight: 16};
+    return (
+      <Page>
+        <InputDate format="dd/mm/yyyy" style={style}/>
+        <InputDate format="yyyy-mm-dd" style={style}/>
+        <InputDate format="yyyy.mm.dd" style={style}/>
+      </Page>
+    )
+  }
+}
+
+export default InputDateDemo;
+
