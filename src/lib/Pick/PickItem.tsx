ab3d801822c10a2e78a136382bc5805081bea76d
Unknown <alexandre.annic1@gmail.com>
2018-06-12 14:20:53 +0200

    Pick: Init

---
+++ src/lib/Pick/PickItem.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import {Checkbox, MenuItem, Radio} from '@material-ui/core';
+import {ReactNode} from 'react';
+
+export interface AutocompleteItemProps {
+  multiple?: boolean
+  disabled?: boolean;
+  checked?: boolean;
+  value: string;
+  children?: ReactNode;
+  onClick?: (value: string) => void
+}
+
+class PickItem extends React.Component<AutocompleteItemProps, {}> {
+
+  render() {
+    const {checked, disabled, multiple, value, children, onClick} = this.props;
+    return (
+      <MenuItem onClick={() => onClick(value)} style={{paddingLeft: 0}} disabled={disabled}>
+        {multiple
+          ? <Checkbox disabled={disabled} checked={checked}/>
+          : <Radio disabled={disabled} checked={checked}/>
+        }
+        {children}
+      </MenuItem>
+    );
+  }
+}
+
+export default PickItem;
