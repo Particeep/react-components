91c19b5af5440b0c652b3c762b9ddda9c8e21490
Alexandre <alexandre.annic1@gmail.com>
2018-11-23 17:55:27 +0100

    Create examples

---
+++ src/demo/examples/Alert/AlertDemoCustom.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react'
+import {Alert, Panel} from '../../../lib/index'
+import {Button} from '@material-ui/core'
+
+export const AlertDemoCustom = () => {
+  return (
+    <Panel>
+      <Alert type="error" icon="cloud_off" action={
+        <Button color="primary" color="error">Dismiss</Button>
+      }>
+        <div style={{fontSize: '1.4em', lineHeight: 1, marginBottom: 8}}>No internet</div>
+        Try
+        <ul>
+          <li>Checking the network cables, modem, and router</li>
+          <li>Reconnecting to Wi-Fi</li>
+          <li>Running Windows Network Diagnostics</li>
+        </ul>
+        <div style={{opacity: .5}}>DNS_PROBE_FINISHED_NO_INTERNET</div>
+      </Alert>
+    </Panel>
+  )
+}
