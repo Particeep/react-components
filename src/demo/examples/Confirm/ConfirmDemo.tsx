ef0c4d4b327e0e3fbd13937cd01ae69caa13056e
Alexandre <alexandre.annic1@gmail.com>
2019-01-16 16:08:35 +0100

    Install new style package to use hook

--- src/demo/examples/Confirm/ConfirmDemo.tsx
+++ src/demo/examples/Confirm/ConfirmDemo.tsx
@@ -6,6 +6,8 @@ import {ConfirmDemoButton} from './ConfirmDemoButton'
 import {ConfirmDemoMenu} from './ConfirmDemoMenu'
 import {Code} from '../../shared/Code/Code'
 import {PageTitle} from '../../shared/PageTitle/PageTitle'
+import {DocTable} from '../../shared/DocTable/DocTable'
+import {ReactNode} from 'react'
 
 const ConfirmDemo = () => {
   return (
@@ -14,6 +16,49 @@ const ConfirmDemo = () => {
       <p>
         Wrap a component with <Code>{`<Confirm/>`}</Code> to pop a confirm dialog before to process a given action.
       </p>
+
+      <h2>API</h2>
+      <DocTable>
+        <thead>
+        <tr>
+          <td>Props</td>
+          <td>Type</td>
+          <td>Description</td>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+          <td>disabled</td>
+          <td>boolean</td>
+        </tr>
+        <tr>
+          <td>title</td>
+          <td>string</td>
+        </tr>
+        <tr>
+          <td>confirmLabel</td>
+          <td>string</td>
+        </tr>
+        <tr>
+          <td>cancelLabel</td>
+          <td>string</td>
+        </tr>
+        <tr>
+          <td>content</td>
+          <td>any</td>
+        </tr>
+        <tr>
+          <td>children</td>
+          <td>ReactNode</td>
+        </tr>
+        <tr>
+          <td>onConfirm</td>
+          <td>() => void</td>
+        </tr>
+        </tbody>
+      </DocTable>
+
+      <h2>Demo</h2>
       <Demo
         raw={preval`module.exports = require('fs').readFileSync(require.resolve('./ConfirmDemoButton.tsx'), 'utf8')`}
         component={ConfirmDemoButton}>
--- src/demo/index.tsx
+++ src/demo/index.tsx
@@ -3,8 +3,11 @@ import * as ReactDOM from 'react-dom'
 import App from './App'
 import {HashRouter} from 'react-router-dom'
 import {ThemeContextProvider, useTheme} from './core/theme/ThemeContext'
-import {createMuiTheme, MuiThemeProvider} from '@material-ui/core'
+import {createMuiTheme} from '@material-ui/core'
+import MuiThemeProvider from '@material-ui/styles/ThemeProvider'
 import {muiTheme} from './core/theme/mui-theme'
+import './core/theme/boostrap'
+
 
 const Root = () => {
   const isDarkTheme = useTheme()
