599a87d0646deae95323eb31fad9789b2d711645
Alexandre Annic <alexandre.annic1@gmail.com>
2019-08-28 15:36:38 +0200

    Fix issue when when using npm link on material-ui

--- doc/webpack.js
+++ doc/webpack.js
@@ -1,6 +1,19 @@
+const path = require('path');
+
 module.exports = {
   resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.json']
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+    alias: {
+      // Why do we need this ?
+      // To use npm link, peerDependency must be also added to devDependency
+      // BUT having multiples react and material-ui instances causes bugs (e.g. https://github.com/facebook/react/issues/13991)
+      // SO using alias seems to prevent using libraries from devDependency of the library.
+      // NOTE that it was not needed before, so maybe I'm missing something.
+      react: path.resolve('./node_modules/react'),
+      'react-dom': path.resolve('./node_modules/react-dom'),
+      '@material-ui/core': path.resolve('./node_modules/@material-ui/core'),
+      '@material-ui/styles': path.resolve('./node_modules/@material-ui/styles')
+    },
   },
   module: {
     rules: [
@@ -24,4 +37,4 @@ module.exports = {
       },
     ]
   },
-}
+};
